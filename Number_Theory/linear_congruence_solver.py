3dd70d70ff27101d65cae8430da98a0ed4663f35
Andrew McNeil <andrew.mcneil1@g.austincc.edu>
2015-05-17 15:19:08 -0500

    linear congruence solver now has more comments

--- Number_Theory/linear_congruence_solver.py
+++ Number_Theory/linear_congruence_solver.py
@@ -1,28 +1,32 @@
 # ax = b mod m
 # m | ax - b or ax-b%m = 0
 
+
+# this function finds all solutions to a linear congruence
 def main():
- print("ax = b mod m")
- a = input("Put in a: ")
- if (a == "x"):
+ print("ax = b mod m") # showing the user the format
+ a = input("Put in a: ") # getting input
+ if (a == "x"): # seeing if the user wants to quit
     return
  a = int(a)
  b = int(input("Put in b: "))
- m = int(input("Put in the modulus: "))
- b = b%m
- a = a%m
- numberOfSolutions = GCD(a, m)
- if (b%numberOfSolutions!=0):
+ m = int(input("Put in m: "))
+ b = b%m # simplifying input
+ a = a%m # simplifying input
+ numberOfSolutions = GCD(a, m) # computing the number of solutions
+ if (b%numberOfSolutions!=0): # if b does not divide GCD(a, m) then there's no solutions
     print ("No Solutions.")
     return
- solutions = []
- for x in range (1, m):
-     if ((a*x-b)%m==0):
-      solutions.append(x)
-      if (len(solutions) == numberOfSolutions):
+ solutions = [] # list for the solutions
+ for x0 in range (1, m): # checking each number in the modulus
+     if ((a*x0-b)%m==0): # checking if the integer x = x0 is a solution
+      solutions.append(x0)
+      if (len(solutions) == numberOfSolutions): # checking to see if we found all of our solutions
         break
- print ("The solutions are:")
- print (solutions)
+ print ("The list of solutions for" , str(a) + "x=" + str(b) + "mod" + str(m) + " are:")
+ print (solutions) # printing out the list of solutions
+ print ("Type 'x' without quotes if you would like to quit. Otherwise, you may continue.")
+ print ("")
  return main()
 
 
