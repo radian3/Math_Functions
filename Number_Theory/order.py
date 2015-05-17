96e2d0e4c8dace8a32c63a41c6edd196c6e64373
Andrew McNeil <andrew.mcneil1@g.austincc.edu>
2015-05-16 23:00:22 -0500

    order function is more efficient

--- Number_Theory/order.py
+++ Number_Theory/order.py
@@ -1,15 +1,41 @@
-
-
-def simp():
- x = -1   
- a = int(input("Put in your constant: "))
- b = int(input("Put in modulus: "))
- for i in range (1, 1000):
-  val = a**i
-  reducedval = (val-(val//b)*b) 
+def main(): # this function computes the order of an integer
+ base = input("Put in your base: ")
+ if (base == "x"):
+     return
+ base = int(base)
+ modulus = int(input("Put in modulus: "))
+ if (GCD(base, modulus) != 1):
+    print ("There is no order because the GCD of", base, "and", modulus, "is not 1")
+    print ("Type 'x' without quotes to quit. Otherwise, you may continue")
+    return main()
+ for exponent in range (1, modulus+1):
+  val = base**exponent
+  reducedval = (val-(val//modulus)*modulus) 
   if (reducedval == 1):
-      x = i
+      index = exponent
       break
- print ("The order is ", x)
- return simp()
-simp()
+
+ print ("The order is ", index)
+ print ("Type 'x' without quotes to quit. Otherwise, you may continue")
+ print ("")
+ return main()
+
+
+
+def GCD(x,y): # Greatest Common Divisor function used in main
+
+ if (x > y):
+    a = x
+    b = y
+ else:
+     a = y
+     b = x
+ q = int(a/b)
+ r = a%b
+ while (r !=0):
+  a = b
+  b = r
+  q = int(a/b)
+  r = a%b
+ return b 
+main()
