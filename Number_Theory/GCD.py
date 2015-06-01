425a31b2fa4b97d1c90f519301cfbcf30a9dba27
Andrew McNeil <andrew.mcneil1@g.austincc.edu>
2015-05-31 20:03:20 -0500

    fixed the GCD bugs and added comments

--- Number_Theory/GCD.py
+++ Number_Theory/GCD.py
@@ -1,48 +1,34 @@
-<<<<<<< HEAD
-def main(): # Greatest Common Divisor calculator
+
+def main(): # Greatest Common Divisor calculator with Euclidean Algorithm
  x = input("Put in the first value: ")
- if (x == "x"):
+ if (x == "x"): # checking to see if the user wants to leave
   return
  x = int(x)
  y = int(input("Put in the second value: "))
  if (x > y): # figuring out which value is bigger
-=======
-def main():
- x = int(input("Put in x: "))
- y = int(input("Put in y: "))
- if (x > y):
->>>>>>> 7449b180171681983dfba1af58b9c7e51c709f1e
     a = x
     b = y
  else:
      a = y
      b = x
 
-<<<<<<< HEAD
- q = int(a/b)  # value
+
+ q = int(a/b)  # quotient
  r = a%b       # remainder	
  print(str(a) + " = " + str(b) + "*" + str(q) + " + " + str(r)) # printing the step
  while (r !=0): # doing more iterations until we have the GCD
-=======
- q = int(a/b)
- r = a%b
- print(str(a) + " = " + str(b) + "*" + str(q) + " + " + str(r))
- while (r !=0):
->>>>>>> 7449b180171681983dfba1af58b9c7e51c709f1e
+
+  q = int(a/b)
+  r = a%b
   a = b
   b = r
   q = int(a/b)
   r = a%b
-<<<<<<< HEAD
+
   print(str(a) + " = " + str(b) + "*" + str(q) + " + " + str(r)) # printing step
  print ("The GCD is", b)
  print ("Type 'x' without the quotes to quit. Otherwise, you may continue")
  print("")
- return main()
-=======
-  print(str(a) + " = " + str(b) + "*" + str(q) + " + " + str(r))
- print ("The GCD is ", b)
->>>>>>> 7449b180171681983dfba1af58b9c7e51c709f1e
- 
+ return main() 
   
 main()
